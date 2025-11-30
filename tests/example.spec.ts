e0c1df9482e7de35cb2fc236f5a2f67f2acf7dd4
Samuel Jurado Quintana <samueljurado12@gmail.com>
2025-11-30 20:31:09 +0100

    Configured project

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Register user", async ({ page }) => {
+  await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  // Missing expect home page is visible
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  const consentButton = await page.getByRole("button", { name: "Consent" });
+  if (await consentButton.isVisible()) {
+    await consentButton.click({ force: true });
+  }
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const signUpLink = page.getByRole("link", { name: "Signup / Login" });
+  await signUpLink.click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  const stringToCheck = "New User Signup!";
+  const locator = page.getByText(stringToCheck);
+  await expect(locator).toBeVisible();
 });
